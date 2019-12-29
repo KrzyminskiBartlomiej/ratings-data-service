8a501587d3c18227f366203871efef2ef71e88a6
bkrzyminski <krzyminski.bartlomiej@gmail.com>
2019-12-29 19:07:56 +0100

    Initioal configuration

---
+++ src/test/java/io/bkrzyminski/ratingsdataservice/RatingsDataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bkrzyminski.ratingsdataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatingsDataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
